8defc10faaeabaa7b0b2f2814ac6e0dd1c876e8d
rinerook <karina.hochstein@gmx.de>
2017-03-23 12:54:32 +0100

    posts show, create, edit & destroy

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,48 @@
 class PostsController < ApplicationController
 	def index
+    @posts = Post.all
   end
+
+  def create
+    @post = Post.new(post_params)
+
+    if @post.save
+      redirect_to posts_path
+    else
+      render "new"
+    end  
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+
+    if @post.update_attributes(post_params)
+      redirect_to posts_path # post_path for single post
+    else
+      render "edit"
+    end
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to posts_path
+  end
+
+  def post_params
+    params.require(:post).permit(:title, :content)
+  end
+
 end
