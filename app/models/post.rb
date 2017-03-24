1de42957f737f70bc1a4ea3774a1340f070ecdcd
rinerook <karina.hochstein@gmx.de>
2017-03-24 12:17:01 +0100

    edit error message

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,6 @@
 class Post < ApplicationRecord
   # validates :title, :content, presence: true # schon im frontend geprüft
-  validates :title, length: { minimum: 6}
+  validates :title, length: { minimum: 6, message: "Titel ist zu kurz!" }
   validates :title, uniqueness: true
   extend FriendlyId
   friendly_id :title, use: :slugged
